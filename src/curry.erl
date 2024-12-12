e35371e6c42077c53e5422030c4c530024c330f3
okayno14 <okayno14@gmail.com>
2024-12-12 16:52:51 +0700

    make_curry может каррировать слева-направо (left), справа-налево(right)

--- src/curry.erl
+++ src/curry.erl
@@ -2,6 +2,7 @@
 
 -export([
     make_curry/1,
+    make_curry/2,
     run_curry/2
 ]).
 
@@ -22,26 +23,38 @@ run_curry(FunCurried, Args) ->
 
 %%--------------------------------------------------------------------
 %% @doc
+-spec make_curry(F :: function()) ->
+    fun() | Result :: term().
+%%--------------------------------------------------------------------
+make_curry(F) ->
+    make_curry(F, left).
+%%--------------------------------------------------------------------
+
+%%--------------------------------------------------------------------
+%% @doc
 %% <pre>
 %% Позволяет выполнить каррирование функции, определённой в модуле:
 %% F :: fun m:f/2 | fun f/2
 %% Также работает и на прочих анонимных функциях.
 %% </pre>
 %% @end
--spec make_curry(F :: function()) ->
+-spec make_curry(F :: function(), Dir :: left | right) ->
     fun() | Result :: term().
 %%--------------------------------------------------------------------
-make_curry(F) when is_function(F) ->
+make_curry(F, Dir) when is_function(F) ->
     Arity = proplists:get_value(arity, erlang:fun_info(F)),
-    fun(X) -> make_curry_(F, Arity, X, []) end.
+    fun(X) -> make_curry_(F, Arity, X, Dir, []) end.
 
-make_curry_(F, _Arity = 1, X, Args) ->
+make_curry_(F, _Arity = 1, X, left, Args) ->
     Args2 = lists:reverse([X | Args]),
     erlang:apply(F, Args2);
 
-make_curry_(F, Arity, X, Args) ->
+make_curry_(F, _Arity = 1, X, _right, Args) ->
+    erlang:apply(F, [X | Args]);
+
+make_curry_(F, Arity, X, Dir, Args) ->
     fun(X1) ->
-        make_curry_(F, Arity - 1, X1, [X | Args])
+        make_curry_(F, Arity - 1, X1, Dir, [X | Args])
     end.
 %%--------------------------------------------------------------------
 

50cb49684e7efc15624ca6f5fb2bf1c1162fb8d5
okayno14 <okayno14@gmail.com>
2025-01-04 02:20:11 +0700

    Убран лишний код в erl_utils

--- src/erl_utils.erl
+++ src/erl_utils.erl
@@ -1,14 +1,4 @@
 -module(erl_utils).
 
--export([main/1]).
-
-main([CMD]) ->
-%     port_lib:run_interactive_cmd("cat -"),
-%     port_lib:run_interactive_cmd("mc"),
-    port_lib:run_interactive_cmd(CMD),
-    ok;
-
-main(_) ->
-    port_lib:run_interactive_cmd("cat -"),
-    ok.
+-export([]).
 
